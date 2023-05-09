6d3167a4f80514fd52db719c67b6520851466c96
hynath <p4homovandrei@yandex.ru>
2023-05-09 20:53:54 +0930

    second commit commint :add files from sprg init

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.NonNull;
+import ru.yandex.practicum.filmorate.validate.Annotation.MinimumDate;
+
+
+import javax.validation.constraints.*;
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+
+public class Film {
+
+    Integer id;
+
+    @NotBlank
+    String name;
+
+    @Size(max = 200,message = "Описание свыше 200 символов")
+    String description;
+
+    @MinimumDate
+    LocalDate releaseDate;
+
+    @Positive
+    Long duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class User {
+    Integer id;
+    @Email
+    String email;
+    @NotBlank
+    String login;
+
+    String name;
+    @PastOrPresent
+    LocalDate birthday;
+
+    public User(Integer id, String email, String login, String name, LocalDate birthday) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        if (name.isBlank())
+            this.name = login;
+        else this.name = name;
+        this.birthday = birthday;
+    }
+}
