52d176485fe123c4037d2c9b8353c6db92ee3ffd
hynath <p4homovandrei@yandex.ru>
2023-05-25 00:45:20 +0930

    10 commit.asd

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.time.LocalDate;
 
 
@@ -20,30 +21,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class FilmorateApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
-	@Test
-	void contextLoads() {
-	}
-	@Test
-	void simpleTest() throws Exception {
-		LocalDate date = LocalDate.of(12,12,12);
-		Film film = new  Film(1,"sd","asd", date,230L);
-		mockMvc.perform( MockMvcRequestBuilders
-						.post("/films")
-						.content(asJsonString(film))
-						.contentType(MediaType.APPLICATION_JSON)
-						.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().is4xxClientError());
-	}
-	public static String asJsonString(final Object obj) {
-		try {
-			final ObjectMapper mapper = new ObjectMapper();
-			mapper.registerModule(new JavaTimeModule());
-			final String jsonContent = mapper.writeValueAsString(obj);
-			return jsonContent;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void simpleTest() throws Exception {
+        LocalDate date = LocalDate.of(12, 12, 12);
+        Film film = new Film(1, "sd", "asd", date, 230L);
+        mockMvc.perform(MockMvcRequestBuilders.post("/films").content(asJsonString(film)).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andExpect(status().is4xxClientError());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
