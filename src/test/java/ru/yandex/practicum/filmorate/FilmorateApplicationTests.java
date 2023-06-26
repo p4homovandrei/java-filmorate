cc21b58ce017e27a87112dfcf4f4d7b280d587cb
hynath <p4homovandrei@yandex.ru>
2023-06-26 13:35:46 +0930

    Main theme 2

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,12 +1,13 @@
 package ru.yandex.practicum.filmorate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -15,11 +16,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.attribute.Genre;
 import ru.yandex.practicum.filmorate.model.attribute.Mpa;
+
 import java.time.LocalDate;
 import java.util.LinkedList;
-import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -39,14 +40,8 @@ class FilmorateApplicationTests {
     @Test
     void simpleTest() throws Exception {
         LocalDate date = LocalDate.of(2012, 12, 12);
-        Film film = new Film(1, "sd", "asd", date, 230L, new LinkedList<Genre>()
-                , new Mpa(1), 1, new LinkedList<Integer>());
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/films")
-                        .content(asJsonString(film))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful());
+        Film film = new Film(1, "sd", "asd", date, 230L, new LinkedList<Genre>(), new Mpa(1), 1, new LinkedList<Integer>());
+        mockMvc.perform(MockMvcRequestBuilders.post("/films").content(asJsonString(film)).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andExpect(status().is2xxSuccessful());
     }
 
     public static String asJsonString(final Object obj) {
